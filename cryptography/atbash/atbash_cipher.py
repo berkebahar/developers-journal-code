df8fa6cce5e55099c086dfdd40e4569d5c39ad5a
Berke <berkebahar228@gmail.com>
2026-01-08 23:20:38 +0300

    Create atbash_cipher.py

---
+++ cryptography/atbash/atbash_cipher.py
@@ -0,0 +1,17 @@
+def atbash_cipher(text):
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    reversed_alphabet = alphabet[::-1]
+    result = ""
+    
+    for char in text.upper():
+        if char in alphabet:
+            index = alphabet.index(char)
+            result += reversed_alphabet[index]
+        else:
+            result += char  
+    return result
+
+
+plain_text = input("Enter text you want to cipher: ") 
+cipher_text = atbash_cipher(plain_text)
+print("Cipher Text:", cipher_text)
